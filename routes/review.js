cc8c12bb265e5604c09bf26ea8a4922417ed8b5c
MuhammadTufail1 <muhammad.tufail@nayatel.com>
2023-12-14 13:56:21 +0500

    first commit

---
+++ routes/review.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router({ mergeParams: true });
+
+const loginController = require('../controller/loginController');
+const reviewController = require('../controller/reviewController');
+
+router
+  .route('/')
+  .post(
+    // loginController.protect,
+    // loginController.restrictTo('user'),
+    // reviewController.setTourUserIds,
+    reviewController.postReview
+  )
+  .get(reviewController.getReviews);
+router
+  .route('/:id')
+  .get(reviewController.getsingleReview)
+  .patch(
+    loginController.protect,
+    loginController.restrictTo('user'),
+    reviewController.updateReview
+  );
+
+router
+  .route('/getProductReview/:id')
+  .get(reviewController.getproductReview)
+
+  .delete(reviewController.deleteReview);
+module.exports = router;
